ebf668a4ff2fc3f6a4414d5fc1b44de4b7c0b0a6
mashroomash <cocomoc95@gmail.com>
2025-02-05 15:30:05 +1000

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WTQ6Y0P5HN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WTQ6Y0P5HN');
+</script>
